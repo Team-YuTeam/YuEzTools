086f4624d7ada8211925a56baade7649818a8445
LezaiYa1 <1792157206@qq.com>
2024-07-08 14:54:55 +0800

    删除多余的using

--- YuAntiCheat/Utils/GetPlayer.cs
+++ YuAntiCheat/Utils/GetPlayer.cs
@@ -1,17 +1,6 @@
 using AmongUs.GameOptions;
-using Hazel;
-using System;
 using System.Linq;
-using HarmonyLib;
-using Il2CppInterop.Runtime.InteropTypes.Arrays;
-using System.Collections.Generic;
-using System.Globalization;
-using System.IO;
-using System.Text;
-using System.Text.RegularExpressions;
 using InnerNet;
-using Sentry.Internal.Extensions;
-using UnityEngine;
 
 namespace YuAntiCheat.Get;
 
