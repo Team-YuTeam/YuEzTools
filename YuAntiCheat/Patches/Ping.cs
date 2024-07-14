91f50426b492c25f1462d6b8ebd14914885cd056
Night-GUA <xiguaos@163.com>
2024-07-15 00:28:20 +0800

    1

--- YuAntiCheat/Patches/Ping.cs
+++ YuAntiCheat/Patches/Ping.cs
@@ -80,8 +80,8 @@ __instance.text.text += "\n<color=#FFC0CB>[DEBUG]</color>";
         
         DateTime dt = TimeZoneInfo.ConvertTimeToUtc(DateTime.Now, TimeZoneInfo.Local);
         DateTime dt1 = TimeZoneInfo.ConvertTimeFromUtc(dt, TimeZoneInfo.FindSystemTimeZoneById("Romance Standard Time"));//参数对应国家或者时区   ***对于有夏令时冬令时的区域，程序会自动调整***
-        if(Toggles.ShowLocalNowTime)__instance.text.text += GetString("Ping.NowTime") + DateTime.Now.ToString();
-        if (Toggles.ShowUTC) __instance.text.text += "\r\n" + "UTC :" + dt.ToString();
+        if(Toggles.ShowLocalNowTime)__instance.text.text += DateTime.Now.ToString();
+        if (Toggles.ShowUTC) __instance.text.text += "\r\n" + "UTC: " + dt.ToString();
 
         
         if (Toggles.ShowRoomTime && GetPlayer.IsLobby && AmongUsClient.Instance.AmHost && GetPlayer.IsOnlineGame)__instance.text.text += "\n<color=#FFD700>"+GameStartManagerPatch.countDown+"</color>";
