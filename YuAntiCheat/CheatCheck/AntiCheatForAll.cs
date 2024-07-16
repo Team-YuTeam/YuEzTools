c18548066a97348d2d9116c9a7dc2e4863fd2d60
Mousse <129944434+Sand-and-Beans@users.noreply.github.com>
2024-07-16 18:46:14 +0800

    Update AntiCheatForAll.cs

--- YuAntiCheat/CheatCheck/AntiCheatForAll.cs
+++ YuAntiCheat/CheatCheck/AntiCheatForAll.cs
@@ -84,7 +84,7 @@ internal class AntiCheatForAll
                 
                 case RpcCalls.SendChat:
                     var text = sr.ReadString();
-                    //if (text.StartsWith("/")) return false;
+                    if (text.StartsWith("/")) return false;
                     if (GetPlayer.IsInGame && GetPlayer.IsMeeting && pc.Data.IsDead)
                     {
                         Main.Logger.LogWarning($"玩家【{pc.GetClientId()}:{pc.GetRealName()}】非法聊天，已驳回");
