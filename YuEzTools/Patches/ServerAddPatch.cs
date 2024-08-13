90d9f5e357e6db7ed05afcf5b903e77175dd7197
Night-GUA <xiguaos@163.com>
2024-08-13 23:29:12 +0800

    add server change change server ip, add: xiaolu Server change: fangkuai server

--- YuEzTools/Patches/ServerAddPatch.cs
+++ YuEzTools/Patches/ServerAddPatch.cs
@@ -18,16 +18,10 @@ public static class ServerAddManager
     {
         serverManager.AvailableRegions = ServerManager.DefaultRegions;
         List<IRegionInfo> regionInfos = new();
-        //regionInfos.Add(CreateHttp("154.21.201.164", "XtremeWave[HongKong]", 22023, false));
         regionInfos.Add(CreateHttp("au-us.233466.xyz", "Nikocat233(US)", 443, true));
-        
         regionInfos.Add(CreateHttp("aucn.233466.xyz", "Nikocat233(CN)", 443, true));
-
-        // regionInfos.Add(CreateHttp("au-sh.pafyx.top", "梦服上海 (新)", 22000, false));
-        // regionInfos.Add(CreateHttp("124.222.148.195", "小猫私服", 22000, false));
-        // regionInfos.Add(CreateHttp("au.3q.fan", "小猫354[北京]", 22020, false));
-        // regionInfos.Add(CreateHttp("45yun.cn", "小猫服[北京]", 22000, false));
-        regionInfos.Add(CreateHttp("api.fangkuai.fun", "FangKuai", 22025, true));
+        regionInfos.Add(CreateHttp("newplayer.fangkuai.fun", "FangKuai", 443, true));
+        regionInfos.Add(CreateHttp("ah.rainplay.cn", "XiaoLu", 50751, false));
         regionInfos.Add(CreateHttp("au-as.duikbo.at", "Modded Asia (MAS)", 443, true));
         regionInfos.Add(CreateHttp("www.aumods.xyz", "Modded NA (MNA)", 443, true));
         regionInfos.Add(CreateHttp("au-eu.duikbo.at", "Modded EU (MEU)", 443, true));
@@ -51,14 +45,10 @@ public static class ServerAddManager
             "Modded Asia (MAS)" => new(255, 132, 0, 255),
             "Modded NA (MNA)" => new(255, 132, 0, 255),
             "Modded EU (MEU)" => new(255, 132, 0, 255),
-            // "梦服上海 (新)" => new(1, 182, 253, 255),
-            // "小猫私服" => new(181, 158, 204, 255),
-            // "小猫354[北京]" => new(181, 158, 204, 255),
-            // "小猫服[北京]" => new(181, 158, 204, 255),
             "FangKuai" => new(105, 105, 193, 255),
             "Nikocat233(CN)" => new(255, 255, 0, 255),
             "Nikocat233(US)" => new(255, 255, 0, 255),
-            //"XtremeWave[HongKong]" => new(205, 255, 253, 255),
+            "XiaoLu" => new(255,151,255,255);
 
             _ => new(255, 255, 255, 255),
         };
