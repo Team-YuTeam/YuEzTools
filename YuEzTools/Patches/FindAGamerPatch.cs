4d57378a57eb9fb40a112668f755cab304b0683b
NightGUA <aaa110910@qq.com>
2025-11-14 23:05:21 +0800

    在GameContainer（原FindAGame)界面新增房主名字、伪装者数、房主平台以及房间代码的显示，v并在Main删除了无用的名字列表

--- YuEzTools/Patches/FindAGamerPatch.cs
+++ YuEzTools/Patches/FindAGamerPatch.cs
@@ -1,6 +1,8 @@
 using Il2CppSystem;
 using InnerNet;
+using TMPro;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace YuEzTools.Patches;
 
@@ -22,31 +24,55 @@ public static class FindAGameManagerUpdatePatch
     }
 }//*/
 
-[HarmonyPatch(typeof(MatchMakerGameButton), nameof(MatchMakerGameButton.SetGame))]
-public static class MatchMakerGameButtonSetGamePatch
+
+[HarmonyPatch(typeof(GameContainer), nameof(GameContainer.SetupGameInfo))]
+public static class SetupGameInfoPatch
 {
-    public static void Prefix(MatchMakerGameButton __instance, [HarmonyArgument(0)] GameListing game)
+    public static GameObject HostName;
+    [HarmonyPostfix]
+    public static void Postfix(GameContainer __instance)
     {
-        var nameList = TranslationController.Instance.currentLanguage.languageID is SupportedLangs.SChinese or SupportedLangs.TChinese ? Main.TName_Snacks_CN : Main.TName_Snacks_EN;
+        var game = __instance.gameListing;
+        // var nameList = TranslationController.Instance.currentLanguage.languageID is SupportedLangs.SChinese or SupportedLangs.TChinese ? Main.TName_Snacks_CN : Main.TName_Snacks_EN;
+     
+        // Some from FS
+        var mapLogo = __instance.mapLogo.transform;
+        var old = mapLogo.parent.FindChild("HostName")?.gameObject;
+        if (old)
+            Object.Destroy(old);
+        var Container = mapLogo.parent.FindChild("Container").gameObject;
+        var Container_TMP = Container.transform.FindChild("Text_TMP").GetComponent<TextMeshPro>();
+        Container_TMP.text += $"<color=#FF0000>({game.NumImpostors})</color>";
+        HostName = new GameObject("HostName")
+        {
+            transform =
+            {
+                parent = __instance.mapLogo.transform.parent,
+                localPosition = new Vector3(-0.6f, mapLogo.localPosition.y - 0.175f, mapLogo.localPosition.z),
+                localScale = new Vector3(0.14f, 0.14f, 1f),
+            },
+        };
 
         if (game.Language.ToString().Length > 9) goto End;
         var color = game.Platform.GetPlatformColor();
         var platforms = game.Platform.GetPlatformText();
-        Info(game.IPString + ":" + game.Port, "FAG");
+        Info(game.IPString + ":" + game.Port, "SetupGameInfoPatch");
         string str = Math.Abs(game.GameId).ToString();
 
-        int id = Math.Min(Math.Max(int.Parse(str.Substring(str.Length - 2, 2)), 1) * nameList.Count / 100, nameList.Count);
-        game.HostName = $"" +
-            $"<size=80%>" +
-            $"<color={color}>" +
-            $"{nameList[id]}" +
-            $"</size>" +
-            $"<size=60%>" +
-            $"({platforms})" +
-            $"</color>" +
-            $"</size>";
-        game.HostName += $"<size=40%> ({GetString("ToCloseThisRoom")}{Math.Max(0, 100 - game.Age / 100)}%)</size>";
-    End:
-        Info("--------This room end.--------", "FindAGamerPatch");
+        // int id = Math.Min(Math.Max(int.Parse(str.Substring(str.Length - 2, 2)), 1) * nameList.Count / 100, nameList.Count);
+        var HNTMP = HostName.AddComponent<TextMeshPro>();
+        HNTMP.text = $"" +
+                     $"<size=45%>" +
+                     $"<color={color}>" +
+                     $"{game.TrueHostName}" +
+                     $"</size>" +
+                     $"<size=25%>" +
+                     $"({platforms})" +
+                     $"</color>" +
+                        $"</size>";
+        HNTMP.text += $"\n<size=18%><color={Main.ModColor}>{GameCode.IntToGameName(game.GameId)}</color>";
+        // HNTMP.alignment = TextAlignmentOptions.MidlineLeft;
+        End:
+        Info("--------This room end.--------", "SetupGameInfoPatch");
     }
 }
